290186af7e4ea604ef86d262f25bbb5deeefd194
GetrudeKatsenga <gkatsenga0527@gmail.comgit config --global user.name>
2024-03-21 16:56:08 +0300

    first commit

---
+++ product.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+        "id": 1,
+        "name": "CEREALS",
+        "price": 200,
+        "description": "Directly imported from Mwea Tebere",
+        "image": "grain.jpg"
+    },
+    {
+        "id": 2,
+        "name": "FRUITS",
+        "price": 200,
+        "description": "Directly imported from Mwea Tebere",
+        "image": "grapes.jpg"
+    },
+    {
+        "id": 3,
+        "name": "VEGETABLES",
+        "price": 200,
+        "description": "Directly imported from Mwea Tebere",
+        "image": "vegetables.jpg"
+    },
+    {
+        "id": 4,
+        "name": "GRAPES",
+        "price": 200,
+        "description": "Directly imported from Mwea Tebere",
+        "image": "grapes.jpg"
+    },
+    {
+        "id": 5,
+        "name": "COCONUT",
+        "price": 200,
+        "description": "Directly imported from Mwea Tebere",
+        "image": "coconut.jpg"
+    },
+    {
+        "id": 6,
+        "name": "LIVESTOCK",
+        "price": 200,
+        "description": "Directly imported from Mwea Tebere",
+        "image": "livestock.jpg"
+    },
+    {
+        "id": 7,
+        "name": "POULTRY",
+        "price": 200,
+        "description": "Directly imported from Mwea Tebere",
+        "image": "roosters.jpg"
+    },
+    {
+        "id": 8,
+        "name": "FISH",
+        "price": 200,
+        "description": "Directly imported from Mwea Tebere",
+        "image": "fish.jpg"
+    }    
+];
+export default products;
